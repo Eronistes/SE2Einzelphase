84bf302c75071ff716f46e79f6b4b5b925c14a35
Benjamin Kupper <beni.kupper@gmail.com>
2020-03-14 16:07:08 +0100

    TCP implementiert via Thread Main Activity: einlesen so wie an TCP schicken("funktioniert noch nicht")

--- app/src/main/java/com/example/se2/MainActivity.java
+++ app/src/main/java/com/example/se2/MainActivity.java
@@ -3,12 +3,60 @@ package com.example.se2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button sendbtn;
+    Button berechnebtn;
+    TextView outputtxt;
+    EditText inputtxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        inputtxt =  findViewById(R.id.inputtxt);
+        outputtxt = findViewById(R.id.outputtxt);
+
+        sendbtn =  findViewById(R.id.abschickenbtn);
+        sendbtn.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           try {
+                                               outputtxt.setText(tcpconnect(inputtxt.getText().toString()));
+                                           } catch (InterruptedException e) {
+                                               e.printStackTrace();
+                                           }
+
+                                       }
+                                   });
+        berechnebtn = findViewById(R.id.berechnebtn);
+        berechnebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+    }
+
+
+    public String tcpconnect(String input) throws InterruptedException {
+        TCP server = new TCP();
+        server.setInput(input);
+        Thread x = new Thread(server);
+        x.start();
+
+        x.join();
+        return server.getOutput();
+
+
     }
 }
--- app/src/main/java/com/example/se2/TCP.java
+++ app/src/main/java/com/example/se2/TCP.java
@@ -1,11 +1,45 @@
 package com.example.se2;
 
+import android.provider.ContactsContract;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class TCP implements Runnable {
-    String input;
-    String output;
+    private String input;
+    private String output;
+
+    String getOutput() {
+        return output;
+    }
+
+    void setInput(String input) {
+        this.input = input;
+    }
+
+
 
     @Override
     public void run() {
+
+        try {
+            Socket clientSocket = new Socket("se2-isys.aau.at",53212);
+            DataOutputStream outUser = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            outUser.writeBytes(input + "/n");
+            System.out.println("Gesendet!");
+            output = inServer.readLine();
+            System.out.println(output);
+            clientSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
